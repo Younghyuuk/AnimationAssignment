3be9b11d78cb6649025deb21c24a1c612d4a6e14
Andrew Chon <andrewyhchon@gmail.com>
2024-01-20 01:12:15 -0800

    fixed slow version

--- slime.js
+++ slime.js
@@ -1,15 +1,19 @@
 class Slime {
     constructor(game) {
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./Run+Attack.png"), 0, 0, 122, 220, 10, 0.2);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./Run+Attack.png"), 0, 0, 129, 90, 10, 0.2);
+        
+        this.x = 0;
+        this.y = 0;
+        this.speed = 25;
     };
 
     update() {
-
+        this.x += this.speed * this.game.clockTick;
     };
 
     draw(ctx) {
-        this.animator.drawFrame(this.game.clockTick, ctx, 25, 25);
+        this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
     };  
 
 }
